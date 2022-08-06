4fff0e90e6ef53d96f8ea1c920b0821da1445fe8
Andrew Kolomiets <andrew.kolomiets.work@gmail.com>
2022-08-06 13:15:22 +0300

    Add 2 problem

---
+++ 2/program.py
@@ -0,0 +1,13 @@
+
+
+print('************************************')
+
+first_name=input('Enter your first-name: ')
+second_name=input('Enter your second-name: ')
+
+print('************************************')
+
+text='Output: {_first_name} {_second_name}'.format(_first_name=first_name,_second_name=second_name)
+print(text)
+
+print('************************************')
