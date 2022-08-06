30ef354d4ae9c4a1ae735e94ab164115e814a662
Andrew Kolomiets <andrew.kolomiets.work@gmail.com>
2022-08-06 20:30:20 +0300

    Correcting any problem

--- 4/program.py
+++ 4/program.py
@@ -3,6 +3,7 @@
 
 input_list=['hqw price','wcwqdjncwd','qcwdcwqwdcprice']
 
-output_list=[x.upper() for x in input_list if 'price' in x]
+
+output_list=[x.upper() if 'price' in x else x for x in input_list  ]
 
 print(output_list)
--- 8/program.py
+++ 8/program.py
@@ -1,18 +1,19 @@
 
-any_list=[44,52,523,62,2,7,23,5,8,3,8,74,234,8734,32,743,4234,654,2542,3434,5,643,64,643,23477,234777,3423452,34523,2,354,23,7,3442,2345,625346,22,5,746,2,52,4,35,346]
+any_list=[x for x in range(700,800)]
+
+print(any_list)
 
 temp=1
 
 for i in any_list:
     
-    if(i==777):
+    if(i==777 and (i in any_list)):
         print("Number 777 is find.")
         break
-        
-    temp+=1
-    
-    if(temp==100 or temp==len(any_list)):
+    elif(temp==100):
         print("Error!")
         break
+     
+    temp+=1
 
     
