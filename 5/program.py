f68577b67651abb2719e0f6bcda7e1eff605b3c8
Andrew Kolomiets <andrew.kolomiets.work@gmail.com>
2022-08-06 23:12:20 +0300

    Add 5 problem

--- 5/program.py
+++ 5/program.py
@@ -1,13 +1,21 @@
 
-list_any=[x for x in range(10)]
+
+
+list_any=[x for x in range(100)]
 
 def split_function(any_list, number):
     if(len(any_list)<number):
         print('Error, input list with size bigger than number')
     else: 
         k, m = divmod(len(any_list), number)
-        return (any_list[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(number))
+        l=[]
+        for i in range(0,len(any_list),k):
+            if(i+k<len(any_list)):
+                l.append(any_list[i:i+k])
+            else:
+                l.append(any_list[i:len(any_list)])
 
-print(split_function(list_any,2))
+        return l
+    
+print(split_function(list_any,10))
 
-# поки не працює треба подумати як реалізувати
