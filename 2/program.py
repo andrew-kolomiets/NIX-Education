40c255938af1c7214db959fd38ebca368851ff95
Andrew Kolomiets <andrew.kolomiets.work@gmail.com>
2022-08-06 20:37:03 +0300

    Add 2 problem

--- 2/program.py
+++ 2/program.py
@@ -8,7 +8,6 @@ second_name=input('Enter your second-name: ')
 print('************************************')
 
 text='Output: {_first_name} {_second_name}'.format(_first_name=first_name,_second_name=second_name)
-
 print(text)
 
 print('************************************')
