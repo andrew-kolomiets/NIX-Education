444e5758849062f1b14e08d772546d8d73529ac4
Andew Kolomiets <andrew.kolomiets.work@gmail.com>
2022-08-15 23:36:01 +0300

    README

--- 20/answer.sql
+++ 20/answer.sql
@@ -1,5 +1,12 @@
 --Вывести наименование группы товаров, общее количество 
 --по группе товаров в порядке убывания количества
 
-SELECT categories.category_title, COUNT(products.in_stock) AS _temp_ FROM products INNER JOIN categories ON categories.category_id = products.category_id
+
+--якщо мова іде про кількість загальна наявних одиниць кожного продукту загалом для кожної групи продуктів
+SELECT categories.category_title, SUM(products.in_stock) AS _temp_ FROM products INNER JOIN categories ON categories.category_id = products.category_id
+GROUP BY categories.category_title ORDER BY _temp_ DESC;
+
+--якщо мається на увазі кількість продуктів в певній категорії
+SELECT categories.category_title, COUNT(products.product_title) AS _temp_ 
+FROM products INNER JOIN categories ON categories.category_id = products.category_id
 GROUP BY categories.category_title ORDER BY _temp_ DESC;
