7dfc9319a4881fc1dd40572cbcf9ab240cf512ed
Andew Kolomiets <andrew.kolomiets.work@gmail.com>
2022-08-18 16:15:14 +0300

    README

--- Advanced level/07/test.py
+++ Advanced level/07/test.py
@@ -1,21 +1,3 @@
-# Напишите функцию, которая делает return
-# строки приветствия людей из разных стран на разных языках
-# , в зависимости от страны человека.
-# Вызовите функцию используя map
-# Требование: используйте map
-# Для примера можете взять этот список юзеров:
-# users_list = [
-#     ('Александр', 'ru'),
-#     ('James', 'us'),
-#     ('Daniella', 'es'),
-#     ('Someone', 'unknown country'),
-# ]
-# На выходе с использованием users_list, после использования map вы должны получить результат iterable с такими приветствиями:
-# Привет, Александр!
-# Hello, James!
-# Hola, Daniella!
-# Hello, Someone, but I do not know where are you from!
-
 def function_hello(tuples:tuple):
     
     if(tuples[1]=='ru'):
